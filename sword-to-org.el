0af23078c6ee4d8577b55f84dd192e2e654237ed
Adam Porter <adam@alphapapa.net>
2017-04-05 13:24:14 -0500

    Get multiple verses at once

--- sword-to-org.el
+++ sword-to-org.el
@@ -1,4 +1,4 @@
-(defconst sword-to-org--diatheke-parse-regexp
+(defconst sword-to-org--diatheke-parse-line-regexp
   (rx bol
       ;; Book name
       (group-n 1 (minimal-match (1+ anything)))
@@ -19,19 +19,43 @@
                                     (end-of-line)
                                     (point)))))
 
-(defun sword-to-org--diatheke-parse-text (text)
-  "Return parsed plist from TEXT."
-  (string-match sword-to-org--diatheke-parse-regexp text)
-  (let ((book (match-string 1 text))
-        (chapter (string-to-number (match-string 2 text)))
-        (verse (string-to-number (match-string 3 text)))
-        (text (s-trim (match-string 4 text))))
-    (list :book book :chapter chapter :verse verse :text text)))
+(defun sword-to-org--diatheke-parse-text (text &optional &key keep-newlines)
+  "Parse TEXT line-by-line, returning list of verse plists.
+When KEEP-NEWLINES is non-nil, keep blank lines in text."
+  (cl-loop with result
+           with new-verse
+           for line in (s-lines text)
+           for parsed = (sword-to-org--diatheke-parse-line line)
+           if parsed
+           do (progn
+                (when new-verse
+                  (push new-verse result))
+                (setq new-verse parsed))
+           else do (let* ((text (plist-get new-verse :text))
+                          (new-text (concat text
+                                            (if (s-present? line)
+                                                line
+                                              (when keep-newlines "\n")))))
+                     (plist-put new-verse :text new-text))
+           finally return (progn
+                            (push new-verse result)
+                            (nreverse result))))
+
+(defun sword-to-org--diatheke-parse-line (line)
+  "Return parsed plist from LINE."
+  (if (s-present? line)
+      (when (string-match sword-to-org--diatheke-parse-line-regexp line)
+        (let ((book (match-string 1 line))
+              (chapter (string-to-number (match-string 2 line)))
+              (verse (string-to-number (match-string 3 line)))
+              (text (s-trim (match-string 4 line))))
+          (list :book book :chapter chapter :verse verse :text text)))))
 
 
 ;; (sword-to-org--diatheke-get-text "ESV" "gen 1:1")
 
-;; (sword-to-org--diatheke-parse-text (sword-to-org--diatheke-get-text "ESV" "gen 1:1"))
+;; (sword-to-org--diatheke-parse-line (sword-to-org--diatheke-get-text "ESV" "gen 1:1"))
 
-;; (cl-loop for verse-number in '(1 2 3)
-;;          collect (sword-to-org--diatheke-parse-text (sword-to-org--diatheke-get-text "ESV" (number-to-string verse-number))))
+;; (cl-loop for verse-number in '(1 2 3) append (sword-to-org--diatheke-parse-text (sword-to-org--diatheke-get-text "ESV" (number-to-string verse-number))))
+
+;; (sword-to-org--diatheke-parse-text (sword-to-org--diatheke-get-text "ESV" "Philemon 1:1-3") :keep-newlines t)
--- test-sword-to-org.el
+++ test-sword-to-org.el
@@ -3,9 +3,23 @@
 (describe "sword-to-org"
   (it "Can get Genesis 1:1-3 from the ESV with Diatheke"
     (expect (cl-loop for verse-number in '(1 2 3)
-                     collect (sword-to-org--diatheke-parse-text
-                              (sword-to-org--diatheke-get-text "ESV" (number-to-string verse-number))))
+                     append (sword-to-org--diatheke-parse-text
+                             (sword-to-org--diatheke-get-text "ESV" (number-to-string verse-number))))
 
             :to-equal '((:book "Genesis" :chapter 1 :verse 1 :text "In the beginning, God created the heavens and the earth.")
                         (:book "Genesis" :chapter 1 :verse 2 :text "The earth was without form and void, and darkness was over the face of the deep. And the Spirit of God was hovering over the face of the waters.")
-                        (:book "Genesis" :chapter 1 :verse 3 :text "And God said, Let there be light, and there was light.")))))
+                        (:book "Genesis" :chapter 1 :verse 3 :text "And God said, Let there be light, and there was light."))))
+
+  (it "Can get Philemon 1-3 from the ESV with Diatheke"
+    (expect (sword-to-org--diatheke-parse-text
+             (sword-to-org--diatheke-get-text "ESV" "Philemon 1:1-3")
+             :keep-newlines t)
+
+            :to-equal '((:book "Philemon" :chapter 1 :verse 1 :text "Paul, a prisoner for Christ Jesus, and Timothy our brother,
+To Philemon our beloved fellow worker")
+                        (:book "Philemon" :chapter 1 :verse 2 :text "and Apphia our sister and Archippus our fellow soldier, and the church in your house:
+
+")
+                        (:book "Philemon" :chapter 1 :verse 3 :text "Grace to you and peace from God our Father and the Lord Jesus Christ.
+
+")))))
